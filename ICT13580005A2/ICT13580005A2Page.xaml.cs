2ccc9f1bf18957cbdd9dff7e95d2a991da9b178c
p2e2t7@gmail.com <Petch0869898562>
2017-12-18 11:53:08 +0700

    A2

---
+++ ICT13580005A2/ICT13580005A2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580005A2
+{
+    public partial class ICT13580005A2Page : ContentPage
+    {
+        public ICT13580005A2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
